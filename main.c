3ca0982d3911aa330694147aeb75a4d3da16ce92
Joshua Franceschi <jfrances@c2r11s4.42yerevan.am>
2023-05-16 14:07:40 +0400

    Added additional structure, set up initialization and basic error function

--- main.c
+++ main.c
@@ -12,21 +12,61 @@
 
 #include "lexer.h"
 
-int main(int ac, char **av, char **envp)
+void    ft_error_exit(char *str)
 {
-    char    *input;
-    t_token    *check;
+    while (*str)
+        write(2, str++, 1);
+    exit(1);
+}
 
-    (void)ac;
-    (void)av;
-    (void)envp;
+void    shell_loop(t_token *tokens)
+{
+    t_token     *check;
+    char        *input;
+
     while ((input = readline("Minishell $")) != NULL)
     {
         add_history(input);
         if (ft_strcmp(input, "exit") == 0)
             break ;
-        check = first_parse(input);
+        check = first_parse(input, tokens);
         free(input);
     }
+}
+
+t_shell    *initalize_shell(t_shell *shell)
+{
+    shell = malloc(sizeof(t_shell));
+    if (!shell)
+        ft_error_exit("Malloc Error: Shell Struct");
+    shell->tokens = malloc(sizeof(t_token));
+    if (!shell->tokens)
+    {
+        free(shell);
+        ft_error_exit("Malloc Error: Tokens Struct");
+    }
+    shell->cmd_tree = malloc(sizeof(t_tree));
+    if (!shell->cmd_tree)
+    {
+        free(shell->tokens);
+        free(shell);
+        ft_error_exit("Malloc Error: ABCT Struct");
+    }
+    shell->tokens = NULL;
+    shell->cmd_tree = NULL;
+    return (shell);
+}
+
+int main(int ac, char **av)
+{
+    t_shell     *shell;
+    
+    (void)ac;
+    (void)av;
+    shell = NULL;
+    if (ac > 1)
+         exit (1);
+    shell = initalize_shell(shell);
+    shell_loop(shell->tokens);
     return (0);
 }
