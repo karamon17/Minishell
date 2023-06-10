47635fdd933e0fcf8d3bd89989744285e49aa10f
karamon17 <gevorik18@gmail.com>
2023-06-10 18:56:51 +0400

    "export" without arguments now works

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: gkhaishb <gkhaishb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 18:31:45 by gkhaishb          #+#    #+#             */
-/*   Updated: 2023/06/09 16:48:41 by gkhaishb         ###   ########.fr       */
+/*   Updated: 2023/06/10 18:54:50 by gkhaishb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,20 @@ void	check_add(t_shell *shell, t_env *env, char *key, char *value)
 	}
 }
 
+void	ft_print_export(t_shell *shell)
+{
+	t_env	*current;
+
+	current = shell->env_lst;
+	while (current)
+	{
+		printf("declare -x ");
+		printf("%s=", current->key);
+		printf("\"%s\"\n", current->value);
+		current = current->next;
+	}
+}
+
 void	ft_export(t_shell *shell, int *flag)
 {
 	char	*value;
@@ -38,6 +52,8 @@ void	ft_export(t_shell *shell, int *flag)
 	*flag = 1;
 	tmp = ft_split(shell->constrs->data, ' ');
 	i = 0;
+	if (!tmp[1])
+		ft_print_export(shell);
 	while (tmp[++i])
 	{
 		if (!ft_strchr(tmp[i], '='))
