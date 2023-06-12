796eedbb1614e51e8bc551977c4fd28b9a4f4b1e
karamon17 <gevorik18@gmail.com>
2023-06-12 18:33:16 +0400

    /bin/ls now works

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: gkhaishb <gkhaishb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 13:11:11 by gkhaishb          #+#    #+#             */
-/*   Updated: 2023/06/12 17:14:47 by gkhaishb         ###   ########.fr       */
+/*   Updated: 2023/06/12 17:57:52 by gkhaishb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,7 +82,10 @@ int	execute(t_shell *shell)
 {
 	char	*str_path;
 
-	str_path = check_path(shell);
+	if (!access(shell->tokens->data, X_OK))
+		str_path = ft_strdup(shell->tokens->data);
+	else
+		str_path = check_path(shell);
 	g_error_status = execute_command(shell, str_path);
 	//free_path(tmp);
 	return (0);
