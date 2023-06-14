cfbbad0361935fb51251248ae9fe1063ce18d1ca
jbfrance <brunofrance18@gmail.com>
2023-06-14 18:17:12 +0400

    norminetted env_str_replace and second_parse, continuing on heredoc

--- src/parser/env_str_replace.c
+++ src/parser/env_str_replace.c
@@ -12,31 +12,30 @@
 
 #include "minishell.h"
 
-char	*get_path(t_shell *shell, char *str)
+static int	set_env(int *i)
+{
+	(*i)++;
+	return (1);
+}
+
+static char	*get_path(t_shell *shell, char *str)
 {
 	str++;
 	str = ft_getenv(shell, str);
 	return (str);
 }
 
-char	*env_in_DQS(t_shell *shell, char *str)
+static char	*env_in_dqs(t_shell *shell, char *str, int i, int is_env)
 {
 	char	*tmp;
 	char	*path;
-	int		i;
-	int		is_env;
 
-	is_env = 0;
-	i = -1;
 	tmp = malloc(sizeof(char));
 	path = malloc(sizeof(char));
 	while (str[++i])
 	{
 		if (str[i] == '$')
-		{
-			is_env = 1;
-			i++;
-		}
+			is_env = set_env(&i);
 		while (is_env == 1 && str[i] != '\0' && str[i] != '"' && str[i] != ' ')
 		{
 			path = ft_strjoin(path, ft_substr(str, i, 1));
@@ -50,7 +49,6 @@ char	*env_in_DQS(t_shell *shell, char *str)
 		}
 		tmp = ft_strjoin(tmp, ft_substr(str, i, 1));
 	}
-	free(path);
 	return (tmp);
 }
 
@@ -62,7 +60,7 @@ t_token	*env_check(t_shell *shell, t_token *tokens)
 	while (tmp)
 	{
 		if (tmp->data[0] == '"')
-			tmp->data = env_in_DQS(shell, tmp->data);
+			tmp->data = env_in_dqs(shell, tmp->data, -1, 0);
 		else if (tmp->data[0] == '$')
 			tmp->data = get_path(shell, tmp->data);
 		tmp = tmp->next;
--- src/parser/second_parse.c
+++ src/parser/second_parse.c
@@ -6,103 +6,103 @@
 /*   By: gkhaishb <gkhaishb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 19:38:27 by jfrances          #+#    #+#             */
-/*   Updated: 2023/06/09 13:35:39 by gkhaishb         ###   ########.fr       */
+/*   Updated: 2023/06/14 18:01:36 by jfrances         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "minishell.h"
+#include "minishell.h"
 
-t_token    *cut_command_quotes(t_token *tokens)
+t_token	*cut_command_quotes(t_token *tokens)
 {
-    int         i;
-    char        *cpy;
-    t_token     *tmp;
-    char        c;
+	int		i;
+	char	*cpy;
+	t_token	*tmp;
+	char	c;
 
-    tmp = tokens;
-    while (tmp != NULL)
-    {   
-        c = tmp->data[0];
-        i = -1;
-        cpy = ft_calloc(1, sizeof(char));
-        while (tmp->data[++i])
-        {
-            if (tmp->data[i] == '\'') // || tmp->data[i] == c)
-                continue ;
-            if (tmp->data[i] == '"') // || tmp->data[i] == c)
-                continue ;
-            cpy = ft_strjoin(cpy, ft_substr(tmp->data, i, 1));      //echoppppp
-        }
-        tmp->data = cpy;
-        tmp = tmp->next;
-    }
-    //free(cpy);
-    return (tokens);
+	tmp = tokens;
+	while (tmp != NULL)
+	{
+		c = tmp->data[0];
+		i = -1;
+		cpy = ft_calloc(1, sizeof(char));
+		while (tmp->data[++i])
+		{
+			if (tmp->data[i] == '\'')
+				continue ;
+			if (tmp->data[i] == '"')
+				continue ;
+			cpy = ft_strjoin(cpy, ft_substr(tmp->data, i, 1));
+		}
+		tmp->data = cpy;
+		tmp = tmp->next;
+	}
+	return (tokens);
 }
 
-void delete_token(t_token **head, t_token *to_delete) 
+void	delete_token(t_token **head, t_token *to_delete)
 {
-    t_token *prevNode;
+	t_token	*prev_node;
 
-    if (*head == NULL || to_delete == NULL)
-        return;
-    if (*head == to_delete) 
-    {
-        (*head)->next = to_delete->next;
-        free(to_delete);
-        return;
-    }
-    prevNode = *head;
-    while (prevNode->next != NULL && prevNode->next != to_delete)
-        prevNode = prevNode->next; 
-    if (prevNode->next == NULL)
-        return;
-    prevNode->next = to_delete->next;
-    free(to_delete);
+	if (*head == NULL || to_delete == NULL)
+		return ;
+	if (*head == to_delete)
+	{
+		(*head)->next = to_delete->next;
+		free(to_delete);
+		return ;
+	}
+	prev_node = *head;
+	while (prev_node->next != NULL && prev_node->next != to_delete)
+		prev_node = prev_node->next;
+	if (prev_node->next == NULL)
+		return ;
+	prev_node->next = to_delete->next;
+	free(to_delete);
 }
 
-t_token *check_options(t_token *tokens) 
+t_token	*check_options(t_token *tokens)
 {
-    t_token *tmp;
-    t_token *to_delete;
-    int foundN;
+	t_token	*tmp;
+	t_token	*to_delete;
+	int		found;
 
-    tmp = tokens;
-    foundN = 0;
-    while (tmp != NULL) 
-    {
-        if (ft_strcmp(tmp->data, "-n") == 0)
-            foundN = 1;
-        else if (foundN) 
-        {
-            if (tmp->data[0] == '-' && ft_strlen(tmp->data) > 1 && strspn(tmp->data, "-")) 
-            {
-                to_delete = tmp;
-                tmp = tmp->next;
-                delete_token(&tokens, to_delete);
-                continue ;
-            }
-        }
-        tmp = tmp->next;
-    }
-    return (tokens);
+	tmp = tokens;
+	found = 0;
+	while (tmp != NULL)
+	{
+		if (ft_strcmp(tmp->data, "-n") == 0)
+			found = 1;
+		else if (found)
+		{
+			if (tmp->data[0] == '-' && ft_strlen(tmp->data) > \
+					1 && strspn(tmp->data, "-"))
+			{
+				to_delete = tmp;
+				tmp = tmp->next;
+				delete_token(&tokens, to_delete);
+				continue ;
+			}
+		}
+		tmp = tmp->next;
+	}
+	return (tokens);
 }
 
-t_token *stugel(t_token *tokens) 
+t_token	*stugel(t_token *tokens)
 {
-    t_token *tmp;
-    int i;
+	t_token	*tmp;
+	int		i;
 
-    tmp = tokens;
-    i = 0;
-    while (tmp != NULL) 
-    {
-        if (ft_strncmp(tmp->data, "echo", 5) == 0 && i == 0)
-            tokens = check_options(tmp);
-        tmp = tmp->next;
-        i++;
-    }
-    tmp = tokens;
-    tokens = cut_command_quotes(tmp);
-    return (tokens);
+	tmp = tokens;
+	i = 0;
+	while (tmp != NULL)
+	{
+		if (ft_strncmp(tmp->data, "echo", 5) == 0 && i == 0)
+			tokens = check_options(tmp);
+		tmp = tmp->next;
+		i++;
+	}
+	tmp = tokens;
+	tokens = cut_command_quotes(tmp);
+	return (tokens);
 }
