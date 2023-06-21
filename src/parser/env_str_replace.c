c196f3ff3dc5c4d7f25b10a35eb2688ebe397ac1
jbfrance <brunofrance18@gmail.com>
2023-06-21 18:53:13 +0400

    fixed error when input == $USER$USER not in quotes, fixed segfault

--- src/parser/env_str_replace.c
+++ src/parser/env_str_replace.c
@@ -12,11 +12,43 @@
 
 #include "minishell.h"
 
-char	*get_path(t_shell *shell, char *str)
+char	*get_path(t_shell *shell, char *str, int i, int is_env)
 {
-	str++;
-	str = ft_getenv(shell, str);
-	return (str);
+	char *tmp;
+    char *path;
+    char *env_value;
+
+    tmp = ft_calloc(1, sizeof(char));
+    path = ft_calloc(1, sizeof(char));
+    while (str[i])
+    {
+        if (str[i] == '$')
+        {
+            is_env = 1;
+            i++;
+        }
+        while (is_env == 1 && str[i] && str[i] != '$')
+        {
+            path = ft_strjoin(path, ft_substr(str, i, 1));
+            i++;
+        }
+        if (is_env == 1)
+        {
+            env_value = ft_getenv(shell, path);
+            tmp = ft_strjoin(tmp, env_value != NULL ? env_value : "");
+            is_env = 0;
+            path = ft_strdup("");
+        }
+        if (str[i] != '\0')
+        {
+            if (str[i] == '$')
+                continue ;
+            tmp = ft_strjoin(tmp, ft_substr(str, i, 1));
+            i++;
+        }
+    }
+    free(path);
+    return tmp;
 }
 
 char *env_in_dqs(t_shell *shell, char *str, int i, int is_env)
@@ -68,7 +100,7 @@ t_token	*env_check(t_shell *shell, t_token *tokens)
 		if (tmp->data[0] == '"')
 			tmp->data = env_in_dqs(shell, tmp->data, 0, 0);
 		else if (tmp->data[0] == '$')
-			tmp->data = get_path(shell, tmp->data);
+			tmp->data = get_path(shell, tmp->data, 0, 0);
 		tmp = tmp->next;
 	}
 	return (tokens);
